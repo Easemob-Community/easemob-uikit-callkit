87f17f007eb7a64869323f697811a50c127355bd
HuangFeiPeng <1094521084@qq.com>
2026-07-16 18:31:13 +0800

    fix(mp-weixin): 修正 demo 主叫跳转路径到 single-call-page

--- packages/callkit-uniapp-mp-weixin/pages/index/index.vue
+++ packages/callkit-uniapp-mp-weixin/pages/index/index.vue
@@ -113,7 +113,7 @@ function startAudioCall() {
     return
   }
   uni.navigateTo({
-    url: `/pages/meeting/meeting?targetUserId=${targetUserId.value}&callType=audio`
+    url: `/uni_modules/easemob-callkit-mp-weixin/pages/single-call-page/single-call-page?targetUserId=${targetUserId.value}&callType=audio`
   })
 }
 
@@ -123,7 +123,7 @@ function startVideoCall() {
     return
   }
   uni.navigateTo({
-    url: `/pages/meeting/meeting?targetUserId=${targetUserId.value}&callType=video`
+    url: `/uni_modules/easemob-callkit-mp-weixin/pages/single-call-page/single-call-page?targetUserId=${targetUserId.value}&callType=video`
   })
 }
 </script>
